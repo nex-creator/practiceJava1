268269fe3656d9cd97f1076e93d124333804c8f6
nex-creator <nehasharma.grace@gmail.com>
2025-01-26 15:36:54 -0500

    newfunctions added

--- src/MathFunctions.java
+++ src/MathFunctions.java
@@ -6,15 +6,21 @@ public class MathFunctions {
 		// TODO Auto-generated method stub
 		int input1 = inputNum();
 	System.out.println("Entered num to calculate factorial:  " + input1);
-	int output = fact(input1);
-	System.out.println("Factorial of " + input1 + " is :" + output);
 	
-	System.out.println("Factorial of " + input1 + " is :" + output);
-	
-
+	System.out.println("To print factorial of given number");
+	fact(input1);
+	System.out.println("to print the fibnoacci sequence");
+	fibnoacci(input1);
+	System.out.println("To check whether a number is prime or not");
+	if (primNo(input1)) {
+		System.out.println(input1 + "is Prime");
+	}
+	else {
+		System.out.println(input1 + "is not Prime");
+	}
 	}
 
-	
+	// for taking input
 	
 	public static int inputNum() {
 		Scanner scanner = new Scanner(System.in);
@@ -23,12 +29,53 @@ public class MathFunctions {
 		return num;
 	}
 	
-	
-	public static int fact(int num) {
+	//calculate factorial of a number
+	public static void fact(int num) {
 		int var =1;
-		for (int i=1;i<= num;i++) {
-			var = var *i;
+		if(num <= 0) {
+			System.out.println("Factorial of" + num+ "is: 0" );
 		}
-		return var;
+		else {	
+			for (int i=1;i<= num;i++) 
+			var = var *i;
+			System.out.println(var);
+		}
+		
 	}
+	
+	// function to print fibnoacci series
+	public static void fibnoacci(int num) {
+		int a=0;
+		int b=1;
+		int c;
+		if(num<=1) {
+			System.out.println(a+ " ");
+		}
+		else {
+			System.out.print(a + " "  +b+ " ");
+			for(int i=3; i<=num;i++) {
+				c = a+b;
+				a = b;
+				b = c;
+				System.out.print(c + " ");		
+			}
+			System.out.println(" ");
+		
+	}
+	}
+	
+	
+	// function to check whether a number is prime
+	
+	public static boolean primNo(int num) {
+		if(num<=1) {
+			return false;
+		}
+		for(int i=2; i < num/2; i++) {
+			if(num%i == 0)
+			return false;
+		}
+		return true;
+		
 }
+	}
