ac9a13a7f72610a9084a411c9a7cb25117658af7
nex-creator <nehasharma.grace@gmail.com>
2025-02-12 18:40:43 -0500

    new function added

---
+++ src/Palindrom.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Palindrom {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+Scanner scanner = new Scanner(System.in);
+System.out.println("Enter the input string to check whether it is palindorme or not");
+String inputString = scanner.nextLine();
+System.out.println("Entered input string is   :   " + inputString);
+boolean isPalindrome = true;
+for(int i=0; i < inputString.length()/2;i++) {
+	if(inputString.charAt(i) !=inputString.charAt(inputString.length()-i-1)) {
+		isPalindrome = false;
+		break;
+	
+	}
+}
+	if(isPalindrome) {
+		System.out.println(inputString + "  is Palindrome");
+	}
+	else
+	{
+		System.out.println(inputString + "  is not Palindrome");
+	}
+	scanner.close();
+	}
+}
